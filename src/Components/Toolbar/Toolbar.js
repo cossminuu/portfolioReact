1a763160c2011e946341eef2e3cd06177d50b284
cossminuu <cossminuu@gmail.com>
2019-12-20 17:49:29 +0100

    nav

--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -4,35 +4,37 @@ import DrawerToggleButton from "../SideDrawer/DrawerToggleButton";
 import { Link } from "react-scroll";
 import data from "../../data";
 
-const toolbar = props => (
-  <header className="toolbar  sticky">
-    <nav className="toolbar__navigation ">
-      <div className="toolbar__toggle__button">
-        <DrawerToggleButton click={props.drawerClickHandler} />
-      </div>
-      <div className="toolbar__navigation__items">
-        <ul>
-          {data.toolbar.map(toolbars => {
-            return (
-              <li key={toolbars.link}>
-                <Link
-                  to={toolbars.to}
-                  spy={true}
-                  smooth={true}
-                  offset={0}
-                  duration={1000}
-                  activeClass="active"
-                  className="hover__effect"
-                >
-                  {toolbars.link}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </nav>
-  </header>
-);
+const toolbar = props => {
+  return (
+    <header className="toolbar  sticky">
+      <nav className="toolbar__navigation ">
+        <div className="toolbar__toggle__button">
+          <DrawerToggleButton click={props.drawerClickHandler} />
+        </div>
+        <div className="toolbar__navigation__items">
+          <ul>
+            {data.toolbar.map(toolbars => {
+              return (
+                <li key={toolbars.link}>
+                  <Link
+                    to={toolbars.to}
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={1000}
+                    activeClass="active"
+                    className="hover__effect"
+                  >
+                    {toolbars.link}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default toolbar;
